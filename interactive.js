f080aa87190bf47ab0f761e15478939ca5e9abd0
Aditya Jain <adityajain@ribkas-iMac.local>
2017-06-02 21:06:02 -0400

    viewBox things

--- interactive.js
+++ interactive.js
@@ -1,9 +1,11 @@
 var theData = d3.csv("USKoreaData.csv",makeStuff);
-var NegoMax = 0;
-	var ProvMax = 0;
+
 
 
 function makeStuff(theData){
+
+	var NegoMax = 0;
+	var ProvMax = 0;
 	
 	for(var i=0;i<theData.length;i++){
 		if(theData[i].Nego>NegoMax){
@@ -22,11 +24,8 @@ function makeStuff(theData){
     	.domain([0,NegoMax])
     	.range(["#eff3ff", "#bdd7e7", "#6baed6","#2171b5"]);
 
-	d3.select('body')
-		.append('svg')
-		.attr('id','calendar')
-		.style('height','900px')
-		.style('width','100%')
+	d3.select('#calendar')
+		.attr('width','100%')
 		.selectAll('g')
 		.data(theData)
 		.enter()
@@ -36,16 +35,22 @@ function makeStuff(theData){
 	d3.selectAll(".row")
 		.append("rect")
 		.attr("class","box")
-		.attr("x",function(d,i){console.log("Nego: "+((30*i)%360)+","+Math.floor(i/12)*15);return ((30*i)%360)})
-		.attr("y",function(d,i){return Math.floor(i/12)*15})
+		.attr("x",function(d,i){return (2*i)%24})
+		.attr("y",function(d,i){return Math.floor(i/12)})
+		.attr('width','1')
+		.attr('height','1')
 		.attr("fill",function(d){if(d.Nego==0){return "white"} else return negoColor(d.Nego)})
 		.text(function(d){return d.Nego;})
 
 	d3.selectAll(".row")
 		.append("rect")
 		.attr("class","box")
-		.attr("x",function(d,i){return (15+(30*i))%375})
-		.attr("y",function(d,i){return Math.floor(i/12)*15})
+		.attr('width','1')
+		.attr('height','1')
+		.attr("x",function(d,i){return 1+(2*i)%24})
+		.attr("y",function(d,i){return Math.floor(i/12)})
 		.attr("fill",function(d){if(d.Prov==0){return "white"} else return provColor(d.Prov)})
 		.text(function(d){return d.Prov;})
 }
+
+document.resize(function(){console.log("sup")})
